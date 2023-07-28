8a44d387934dd5a6b655caa9d8b3489306ae86db
Rafael Constantino <rafael.constantino89@gmail.com>
2023-07-28 13:51:34 -0300

    inital commit

---
+++ teste-tecnico-fadami/teste-tecnico-fadami/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace teste_tecnico_fadami.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
