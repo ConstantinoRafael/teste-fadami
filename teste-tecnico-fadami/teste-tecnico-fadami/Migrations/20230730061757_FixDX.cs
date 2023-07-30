f056f5b6a57c0f1535668289e281988d94d28eff
Rafael Constantino <rafael.constantino89@gmail.com>
2023-07-30 15:55:22 -0300

    fix: new migration fixing usuario model

---
+++ teste-tecnico-fadami/teste-tecnico-fadami/Migrations/20230730061757_FixDX.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace teste_tecnico_fadami.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDX : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ULTIMO_ACESSO",
+                table: "Usuarios",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SENHA",
+                table: "Usuarios",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QTD_ERRO_LOGIN",
+                table: "Usuarios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NOME",
+                table: "Usuarios",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LOGIN",
+                table: "Usuarios",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Usuarios",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "BL_ATIVO",
+                table: "Usuarios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ULTIMO_ACESSO",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SENHA",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QTD_ERRO_LOGIN",
+                table: "Usuarios",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NOME",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LOGIN",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CPF",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BL_ATIVO",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
--- teste-tecnico-fadami/teste-tecnico-fadami/Models/UsuarioModel.cs
+++ teste-tecnico-fadami/teste-tecnico-fadami/Models/UsuarioModel.cs
@@ -1,15 +1,26 @@
-﻿namespace teste_tecnico_fadami.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace teste_tecnico_fadami.Models
 {
     public class UsuarioModel
     {
         public int Id {get; set; }
-        public string? NOME { get; set; }
-        public string? LOGIN { get; set; }
-        public string? CPF { get; set; }
-        public string? SENHA { get; set; }
-        public string? ULTIMO_ACESSO { get; set; }
-        public int? QTD_ERRO_LOGIN { get; set; }
-        public string? BL_ATIVO { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string NOME { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string LOGIN { get; set; }
+        [Required]
+        [MaxLength(14)]
+        public string CPF { get; set; }
+        [Required]
+        [MinLength(7)]
+        [MaxLength(20)]
+        public string SENHA { get; set; }
+        public DateTime ULTIMO_ACESSO { get; set; }
+        public int QTD_ERRO_LOGIN { get; set; }
+        public bool BL_ATIVO { get; set; }
 
     }
 }
